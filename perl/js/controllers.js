994837d0b4a51fc0d6c6e36b97cabccc2c985987
Vlastimil Holer <holer@ics.muni.cz>
2013-11-13 02:21:49 +0100

    Format date of last update

--- perl/js/controllers.js
+++ perl/js/controllers.js
@@ -13,6 +13,7 @@ function SensorListCtrl($scope, $http) {
 function SensorDetailCtrl($scope, $routeParams, $http) {
   $http.get('sensor/' + $routeParams.id + '.json').success(function(data) {
     $scope.sensor = data;
+    $scope.dateFmt = new Date(data.date * 1000).format('h:i:s');
   });
 }
 //SensorDetailCtrl.$inject = ['$scope', '$routeParams'];
