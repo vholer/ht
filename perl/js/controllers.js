1454f6703532b877ad2ba0200d7589eee4989647
Vlastimil Holer <holer@ics.muni.cz>
2013-11-13 02:14:22 +0100

    Revert relative URIs, fix dependency on $http

--- perl/js/controllers.js
+++ perl/js/controllers.js
@@ -3,15 +3,15 @@
 /* Controllers */
 
 function SensorListCtrl($scope, $http) {
-  $http.get('/sensor/.json').success(function(data) {
+  $http.get('sensor/.json').success(function(data) {
     $scope.sensors = data;
   });
 }
 //SensorListCtrl.$inject = ['$scope', '$http'];
 
 
-function SensorDetailCtrl($scope, $routeParams) {
-  $http.get('/sensor/' + $routeParams.id + '.json').success(function(data) {
+function SensorDetailCtrl($scope, $routeParams, $http) {
+  $http.get('sensor/' + $routeParams.id + '.json').success(function(data) {
 	$scope.sensor = data;
     $scope.id = $routeParams.id;
   });
